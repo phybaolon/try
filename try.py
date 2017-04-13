553cb1da77453c87b1090e0529121e5b21580432
phybaolon <baolongxu@pku.edu.cn>
2017-04-13 14:50:05 +0800

    Update try.py

--- try.py
+++ try.py
@@ -52,31 +52,15 @@ def main():
     k=50
     topk=[]
     # firstly, get top 50 elements 
-    for id in idlist:
-        idnum=idlist.index(id)
-        temp=[]
-        if len(scorelist[idnum])<k:
-            temp=scorelist[idnum]
+    for idnum in range(0,len(idlist)):
+        #
+        temp=scorelist[idnum]
+        temp1=[]
+        if len(temp)<k:            
             temp1=sorted(temp,key=lambda a_tuple:a_tuple[1],reverse=True)
-        else:
-            for i in range(0,k):
-                temp.append(scorelist[idnum][i])
-            temp1=sorted(temp,key=lambda a_tuple:a_tuple[1],reverse=True)
-            for j in range(k,len(scorelist[idnum])):
-                tet=scorelist[idnum][j]            
-                if tet[1]>temp1[k-1][1]:
-                    temp1[k-1]=tet
-                    sensor=k-2
-                    while sensor>0:
-                        if temp1[sensor][1]<temp1[sensor+1][1]:
-                            q=temp1[sensor]
-                            temp1[sensor]=temp1[sensor+1]
-                            temp1[sensor+1]=q
-                            sensor=sensor-1
-                        #print sensor
-                        else:
-                            sensor=0                
-        topk.append(temp1)              
+        else:            
+            temp1=sorted(temp,key=lambda a_tuple:a_tuple[1],reverse=True)[:50]              
+        topk.append(temp1)       
         #topk is a list of tuple.
     
     #print topk
