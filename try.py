3d4eecbae7b356f8e1f911186f970edf866cc487
phybaolon <baolongxu@pku.edu.cn>
2017-04-13 10:14:05 +0800

    Update try.py

--- try.py
+++ try.py
@@ -55,24 +55,27 @@ def main():
     for id in idlist:
         idnum=idlist.index(id)
         temp=[]
-        for i in range(0,k):
-            temp.append(scorelist[idnum][i])
-        temp1=sorted(temp,key=lambda a_tuple:a_tuple[1],reverse=True)
-        for j in range(k,len(scorelist[idnum])):
-            tet=scorelist[idnum][j]            
-            if tet[1]>temp1[k-1][1]:
-                temp1[k-1]=tet
-                sensor=k-2
-                while sensor>0:
-                    if temp1[sensor][1]<temp1[sensor+1][1]:
-                        q=temp1[sensor]
-                        temp1[sensor]=temp1[sensor+1]
-                        temp1[sensor+1]=q
-                        sensor=sensor-1
+        if len(scorelist[idnum])<k:
+            temp=scorelist[idnum]
+            temp1=sorted(temp,key=lambda a_tuple:a_tuple[1],reverse=True)
+        else:
+            for i in range(0,k):
+                temp.append(scorelist[idnum][i])
+            temp1=sorted(temp,key=lambda a_tuple:a_tuple[1],reverse=True)
+            for j in range(k,len(scorelist[idnum])):
+                tet=scorelist[idnum][j]            
+                if tet[1]>temp1[k-1][1]:
+                    temp1[k-1]=tet
+                    sensor=k-2
+                    while sensor>0:
+                        if temp1[sensor][1]<temp1[sensor+1][1]:
+                            q=temp1[sensor]
+                            temp1[sensor]=temp1[sensor+1]
+                            temp1[sensor+1]=q
+                            sensor=sensor-1
                         #print sensor
-                    else:
-                        sensor=0
-                                                      
+                        else:
+                            sensor=0                
         topk.append(temp1)              
         #topk is a list of tuple.
     
