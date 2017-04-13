95f38eb6f5ce1f20c4edf34dc8d35545b02aa60f
phybaolon <baolongxu@pku.edu.cn>
2017-04-13 15:41:11 +0800

    Update try.py

--- try.py
+++ try.py
@@ -54,13 +54,29 @@ def main():
     # firstly, get top 50 elements 
     for idnum in range(0,len(idlist)):
         #
-        temp=scorelist[idnum]
-        temp1=[]
-        if len(temp)<k:            
+        temp=[]
+        if len(scorelist[idnum])<k:
+            temp=scorelist[idnum]
             temp1=sorted(temp,key=lambda a_tuple:a_tuple[1],reverse=True)
         else:            
-            temp1=sorted(temp,key=lambda a_tuple:a_tuple[1],reverse=True)[:50]              
-        topk.append(temp1)       
+            for i in range(0,k):
+                temp.append(scorelist[idnum][i])
+            temp1=sorted(temp,key=lambda a_tuple:a_tuple[1],reverse=True)
+            for j in range(k,len(scorelist[idnum])):
+                tet=scorelist[idnum][j]            
+                if tet[1]>temp1[k-1][1]:
+                    temp1[k-1]=tet
+                    sensor=k-2
+                    while sensor>0:
+                        if temp1[sensor][1]<temp1[sensor+1][1]:
+                            q=temp1[sensor]
+                            temp1[sensor]=temp1[sensor+1]
+                            temp1[sensor+1]=q
+                            sensor=sensor-1
+                        #print sensor
+                        else:
+                            sensor=0                
+        topk.append(temp1)              
         #topk is a list of tuple.
     
     #print topk
@@ -86,7 +102,6 @@ def main():
 
 if __name__ == '__main__':
     main()
-
 
 
 
