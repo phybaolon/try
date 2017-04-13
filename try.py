1deb32b1029f121be94c28a84f916bf68507558c
phybaolon <baolongxu@pku.edu.cn>
2017-04-13 09:53:39 +0800

    Update try.py

--- try.py
+++ try.py
@@ -4,6 +4,12 @@ from os import listdir
 import sys
 import math
 import operator
+import time
+
+
+start = time.clock()
+
+
 #import heapq
 
 INFILE_LIST = '/data/face_attributes/130-139_1face_attributes.txt'
@@ -30,7 +36,10 @@ def main():
         scoresplit=scoreline.strip().split(' ')
         scorfo=float(scoresplit[1])
         
-        temp1=(infoline.strip(),scorfo)        
+        temp1=(infoline.strip(),scorfo)
+        #temp1['name']=prop.split('_')[0] 
+        #temp1['name']=infoline.strip()           
+        
         
         if info not in idlist:
             idlist.append(info)
@@ -40,25 +49,30 @@ def main():
             tid=idlist.index(info)            
             scorelist[tid].append(temp1)               
     
+    k=50
     topk=[]
     # firstly, get top 50 elements 
     for id in idlist:
         idnum=idlist.index(id)
         temp=[]
-        for i in range(0,50):
+        for i in range(0,k):
             temp.append(scorelist[idnum][i])
         temp1=sorted(temp,key=lambda a_tuple:a_tuple[1],reverse=True)
-        for j in range(51,len(scorelist[idnum])):
+        for j in range(k,len(scorelist[idnum])):
             tet=scorelist[idnum][j]            
-            if tet[1]>temp1[0][1]:
-                temp1.insert(0, tet)
-            elif ((tet[1]>temp1[49][1]) and (tet[1]<temp1[0][1])):
-                inser=48
-                while inser>0:                                       
-                    if temp1[inser][1]>tet:
-                        temp1.insert(inser+1,tet)
-                        continue
-                    inser=inser-1                 
+            if tet[1]>temp1[k-1][1]:
+                temp1[k-1]=tet
+                sensor=k-2
+                while sensor>0:
+                    if temp1[sensor][1]<temp1[sensor+1][1]:
+                        q=temp1[sensor]
+                        temp1[sensor]=temp1[sensor+1]
+                        temp1[sensor+1]=q
+                        sensor=sensor-1
+                        #print sensor
+                    else:
+                        sensor=0
+                                                      
         topk.append(temp1)              
         #topk is a list of tuple.
     
@@ -68,9 +82,24 @@ def main():
             print id[i][0]
             print id[i][1]
             
-            
+    end = time.clock()
+    print "read: %f s" % (end - start)        
+        
+        
+    
+    # then made some choices between the names of the figures.
+    
+        
+    
+    
+    
+    
+   
+        
+
 if __name__ == '__main__':
     main()
+
 
 
 
